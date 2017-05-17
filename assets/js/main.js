09a112afadbcfbecef3b1a492aaa86abec03b29d
lenzzz <401738000@qq.com>
2017-05-17 12:42:15 +0800

    add eslint

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,10 @@
 /**
  * Created by wuhow on 2017/5/12.
  */
-"using strict"
-let toggleClick = function () {
-
-    let checkDisply = function (value, lists) {
-        let length = lists.length;
+"using strict";
+function toggleClick() {
+    function checkDisplay(value, lists) {
+        const length = lists.length;
         for (let i = 0; i < length; i++) {
             if (value === lists[i])
                 return false;
@@ -13,35 +12,33 @@ let toggleClick = function () {
         return true;
     };
 
-    let site_info_dom = document.getElementById("site-info");
-    let footer_dom = document.getElementById("footer");
-    let articles_dom = document.getElementById("articles");
-    let main_body_dom = document.getElementById("main-body");
+    const site_info_dom = document.getElementById("site-info");
+    const footer_dom = document.getElementById("footer");
+    const articles_dom = document.getElementById("articles");
+    const main_body_dom = document.getElementById("main-body");
 
 
-    if (checkDisply("hidden-block", site_info_dom.classList)) {
+    if (checkDisplay("hidden-block", site_info_dom.classList)) {
         site_info_dom.classList.add("hidden-block");
-        if (footer_dom != null)
+        if (null !== footer_dom)
             footer_dom.classList.remove("hidden-block");
-        if (articles_dom != null)
+        if (null !== articles_dom)
             articles_dom.classList.remove("hidden-block");
-        if (main_body_dom != null)
+        if (main_body_dom !== null)
             main_body_dom.classList.remove("hidden-block");
     }
     else {
         site_info_dom.classList.remove("hidden-block");
-        if (footer_dom != null)
+        if (null !== footer_dom)
             footer_dom.classList.add("hidden-block");
-        if (articles_dom != null)
+        if (null !== articles_dom)
             articles_dom.classList.add("hidden-block");
-        if (main_body_dom != null)
+        if (null !== main_body_dom)
             main_body_dom.classList.add("hidden-block");
     }
-
-
 };
 
-document.getElementById("toggle-button").addEventListener("click", function () {
+document.getElementById("toggle-button").addEventListener("click", () => {
     toggleClick();
 });
 
